a7d57c1e8dff86916ef6951644167cc0e9d5ae1a
Badr <badrbiyadi@github.com>
2021-02-13 16:46:19 +0100

    Add the products relationship for the category

--- nextmediaChallenge/app/Category.php
+++ nextmediaChallenge/app/Category.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public function parent() {
+    public function parent() 
+    {
         return $this->hasOne(Category::class, 'parent_id');
     }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+    
 }
