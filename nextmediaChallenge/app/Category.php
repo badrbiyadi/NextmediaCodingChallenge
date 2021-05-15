0b67a2e8d09a51519a2839e03afecc522821b23a
Badr <badrbiyadi@github.com>
2021-05-15 19:13:47 +0000

    Add the fields to the fillable array

--- nextmediaChallenge/app/Category.php
+++ nextmediaChallenge/app/Category.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = [
+        'name',
+        'parent_id',
+    ];
+
     public function parent() 
     {
         return $this->hasOne(Category::class, 'parent_id');
--- nextmediaChallenge/app/Product.php
+++ nextmediaChallenge/app/Product.php
@@ -5,7 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
-{
+{   
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image',
+    ];
+    
     public function categories() {
         return $this->belongsToMany(Category::class);
     }
