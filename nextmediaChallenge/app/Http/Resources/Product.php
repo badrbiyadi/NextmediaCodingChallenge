24d1faadca3d012152d8956159e035175f626f96
Badr <badrbiyadi@github.com>
2021-05-16 00:01:27 +0000

    adding the resources layer

---
+++ nextmediaChallenge/app/Http/Resources/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Product extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'date' => $this->created_at,
+            'categories' => $this->categories,
+        ];
+    }
+}
